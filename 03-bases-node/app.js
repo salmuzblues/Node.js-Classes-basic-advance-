a027f3b8e5f56392b96e23a63f806f7d091a8a62
“salmuzblues” <salmuzblues@gmail.com>
2018-10-17 23:43:16 -0500

    inserting values by console (terminal) using process.argv

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,8 +1,21 @@
 // calling method.
 const { crearArchivo } = require('./multiplicar/multiplicarApp.js');
 
-let base = 'abc';
+//let base = 'abc';
 
+
+// console.log(process.argv);
+// this code is for giving data for terminal
+
+let argv = process.argv;
+let parametro = argv[2];// split is for changing String to array the argument is base=5 then the result is
+                        //[ base, 5]
+let base = parametro.split('=')[1];
+/*
+NOTA IMPORTANTE : when you want to send values if you write like this hasta=20, hence it would
+work the same because that only would care it is value 20, so it would create the table X 20.
+or you write whatever it is the same. becareful
+ */
 // retrieve  a promise
 // if I want just to use the entire name of the method, no to call by name constan like this
 // multiplicar.crearArchivo, we need to destructurar
