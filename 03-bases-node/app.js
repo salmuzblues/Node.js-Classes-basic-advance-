86518f737748e2285768e1b39876592adc61ae03
“salmuzblues” <salmuzblues@gmail.com>
2018-10-18 12:51:10 -0500

    adding file json and using yargs for making commands and flags --b

--- 03-bases-node/app.js
+++ 03-bases-node/app.js
@@ -1,16 +1,34 @@
 // calling method.
 const { crearArchivo } = require('./multiplicar/multiplicarApp.js');
-
+const argv = require('yargs')
+                    .command('listar', 'print by console  a table o multiply',{
+                     base: {
+                      demand: true,
+                      alias: 'b'
+                     },
+                      limite:{
+                      alias: 'l',
+                      default: 10
+                      }
+                    })
+                    .help()
+                    .argv;
 //let base = 'abc';
 
 
 // console.log(process.argv);
 // this code is for giving data for terminal
 
-let argv = process.argv;
+//let argv2 = process.argv;
+
+    console.log('Base: ', argv.base);
+    console.log('Limite: ',argv.limite);
+ //   console.log(argv2);
+/*
 let parametro = argv[2];// split is for changing String to array the argument is base=5 then the result is
                         //[ base, 5]
 let base = parametro.split('=')[1];
+*
 /*
 NOTA IMPORTANTE : when you want to send values if you write like this hasta=20, hence it would
 work the same because that only would care it is value 20, so it would create the table X 20.
@@ -19,5 +37,5 @@ or you write whatever it is the same. becareful
 // retrieve  a promise
 // if I want just to use the entire name of the method, no to call by name constan like this
 // multiplicar.crearArchivo, we need to destructurar
- crearArchivo(base).then(archivo => console.log(`Archivo creado ${archivo}`))
-                    .catch(e => console.log(e));
+// crearArchivo(base).then(archivo => console.log(`Archivo creado ${archivo}`))
+ //                   .catch(e => console.log(e));
