8e7e2a22cda10027bf96fd7d2b64559577504073
“salmuzblues” <salmuzblues@gmail.com>
2018-11-16 14:45:40 -0500

    Finishing the proyect todo

---
+++ 04-por-hacer/README.md
@@ -0,0 +1,9 @@
+## Aplicación de Comandos 
+Este es el ejercicio del curso UDEMY 
+
+Recordar instalar los paquetes de node 
+
+    ````
+    npm install
+    
+    ```
--- 04-por-hacer/config/yargs-conf.js
+++ 04-por-hacer/config/yargs-conf.js
@@ -1,26 +1,23 @@
+const descripcion = {
+    demand: true,
+    alias: 'd',
+    desc:'Descripcion por la tarea por hacer'
+};
 
+const completado = {
+    alias: 'c',
+    default: true,
+    desc:'Marca completado o pendiente la tarea'
+};
 const argv = require('yargs')
-                .command('crear', 'crear un elemento', {descripcion: {
-                     demand: true,
-                         alias: 'd',
-                    desc:'Descripcion por la tarea por hacer'
-                    }})
+                .command('crear', 'crear un elemento', {
+                    descripcion
+                    })
                 .command('actualizar','Actualiza el estado completado de una tarea', {
-                    descripcion: {
-                        demand: true,
-                        alias: 'd',
-                        desc:'Descripcion por la tarea por hacer'},
-                    completado:{
-                        alias: 'c',
-                        default: true,
-                        desc:'Marca completado o pendiente la tarea'
-                    }
+                    descripcion,
+                    completado
                 }).command('borrar', 'borra una tarea', {
-                    descripcion: {
-                     demand: true,
-                     alias: 'd',
-                     desc: 'Descripcion por la tarea por hacer'
-                 }
+                    descripcion
                 }).help().argv;
 
 //exports variables
