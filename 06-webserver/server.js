c8ad80b6ed208877dd6ef4bb8788182c5c76985b
“salmuzblues” <salmuzblues@gmail.com>
2018-12-11 16:23:02 -0500

    working with boostrap css and js

--- 06-webserver/server.js
+++ 06-webserver/server.js
@@ -1,6 +1,10 @@
 const express = require('express');
 const app = express();
 
+// middleware es una instrucción o callback que se va a ejecutar siempre que url pida
+app.use(express.static( __dirname  + '/public'));
+
+/*
 app.get('/', function (req, res) {
    // res.send('Hello world');
     let salida = {
@@ -10,7 +14,7 @@ app.get('/', function (req, res) {
     };
 
     res.send(salida);
-});
+}); */
 app.listen(3000, () => {
     console.log('Escichando peticiones en el puerto numero 3000');
 });
